471cc6008e39cb82f4eb51a1f224c144516673ec
Juan Rios <juanrios@tuta.io>
2022-01-26 19:10:40 -0500

    add: .env and model.js files added to define database scheme and acess to mongodb

--- nodejs-rest-api/index.js
+++ nodejs-rest-api/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB!');
+})
+.catch ((err) => {
+    console.log(err);
+});
+
+
 const app = express();
+
+//Adding a get HTTP method to anwer with hello world!
+app.get('/', function(req, res) {
+    res.send('Hello world!');
+});
 
 const port = process.env.PORT || 3000;
 
